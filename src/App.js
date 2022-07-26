f05693017c0081d8e837b950e32e796d400c297d
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-07-26 19:30:14 -0400

    created web form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>JS Form Validation</h1>
+      </header>
+      <main>
+        <form action='https://cs12.net/form/submit.php' method='post'>
+          <label>Name<input type='text' name='name'></input></label>
+          <label>Email<input type='email' name='email'></input></label>
+          <label>Feedback<textarea name='feedback' cols='30' rows='10'></textarea></label>
+          <button type='submit'>Submit</button>
+        </form>
+      </main>
+      <footer><p>Website Created by Christian Isaman for CSCI S-12 using React</p></footer>
     </div>
   );
 }

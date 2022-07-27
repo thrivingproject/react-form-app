0ed1868c955581cdcfc404e6a0498568d797b59b
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-07-27 08:08:55 -0400

    beginning form validation using Yup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import './site.css'
+import * as yup from 'yup';
+import {userSchema} from './validations/user-validation'
 
 function App() {
+
+  const createUser = (event) => {
+    event.preventDefault()
+    let formData = {
+      name: event.target[0].value,
+      email: event.target[1].value,
+      password: event.target[2].value
+    };
+    console.log(formData);
+  }
+
   return (
     <div className="App">
+
       <header>
         <h1>JS Form Validation</h1>
       </header>
+
       <main>
-        <form action='https://cs12.net/form/submit.php' method='post'>
+        <form action='https://cs12.net/form/submit.php' method='post' onSubmit={createUser}>
+
           <label>Name<input type='text' name='name'></input></label>
           <label>Email<input type='email' name='email'></input></label>
-          <label>Feedback<textarea name='feedback' cols='30' rows='10'></textarea></label>
+          <label>Password<input type='password' name='password'></input></label>
           <button type='submit'>Submit</button>
+
         </form>
       </main>
-      <footer><p>Website Created by Christian Isaman for CSCI S-12 using React</p></footer>
+
+      <footer>
+        <p>Website Created by Christian Isaman for CSCI S-12 using ReactJS</p>
+      </footer>
+
     </div>
   );
 }

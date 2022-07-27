ad9fdf242dd54d52f48824b1cc0b8964eb297de3
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-07-27 11:29:33 -0400

    Form validates data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './site.css'
+import { useForm } from 'react-hook-form'
 
 function App() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  /* This does not work, need to figure out how to post data to endpoint */
+  const onSubmit = (data) => {
+    fetch('https://cs12.net/form/submit.php', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+  }
 
   return (
     <div className="App">
@@ -10,14 +21,32 @@ function App() {
       </header>
 
       <main>
-        <form action='https://cs12.net/form/submit.php' method='post'>
 
-          <label>Name<input type='text' name='name'></input></label>
-          <label>Email<input type='email' name='email'></input></label>
-          <label>Password<input type='password' name='password'></input></label>
-          <button type='submit'>Submit</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+          <label>
+            First Name
+            <input {...register('firstName', {required: 'This is required'})} type='text'/>
+            <p>{errors.firstName?.message}</p>
+          </label>
+
+          <label>
+            Last Name
+            <input {...register('lastName', {required: 'This is required'})} type='text'/>
+            <p>{errors.lastName?.message}</p>
+          </label>
+
+          <label>
+            Email
+            <input {...register('email', {required: 'This is required'})} type='email'/>
+            <p>{errors.email?.message}</p>
+          </label>
+
+          {/* <button type='submit'>Submit</button> */}
+          <input type='submit'/>
 
         </form>
+
       </main>
 
       <footer>
